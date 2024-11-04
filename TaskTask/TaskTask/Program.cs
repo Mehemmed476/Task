d0f3bd148ba419eba7f559bab52f500fdc0c128d
XalilzadeMehemmed <mahammadax-ab205@code.edu.az>
2024-11-04 23:05:24 +0400

    ok

---
+++ TaskTask/TaskTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace TaskTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        
+        List<string> urls = new List<string>()
+        {
+            "https://www.microsoft.com/en-us/",
+            "https://www.meta.com",
+            "https://www.freelancer.com"
+        };
+        
+        RetrieveHTMLData(urls);
+        Console.WriteLine($"Completed in {sw.ElapsedMilliseconds} ms.");
+        
+        Console.WriteLine("------------------------------------------------------------------");
+        
+        sw.Restart();
+        
+        RetrieveHTMLDataAsync(urls).Wait();
+        Console.WriteLine($"Completed in {sw.ElapsedMilliseconds} ms.");
+
+    }
+
+    public static void RetrieveHTMLData(List<string> urls)
+    {
+        HttpClient client = new HttpClient();
+
+        foreach (var item in urls)
+        {
+            client.GetStringAsync(item).Result.ToString();
+        }
+    }
+
+    public static async Task RetrieveHTMLDataAsync(List<string> urls)
+    {
+        HttpClient client = new HttpClient();
+
+        List<Task<string>> tasks = new List<Task<string>>();
+        
+        foreach (var item in urls)
+        {
+            tasks.Add(client.GetStringAsync(item));
+        }
+        
+        await Task.WhenAll(tasks);
+    }
+}
